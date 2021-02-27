cfb4d5ccf50211f111068f65b17e8a4cd5c7e3b6
Luis Cereda <62411006+lhsaia@users.noreply.github.com>
2021-02-27 20:09:19 -0300

    2021 footer update

--- elements/footer.php
+++ elements/footer.php
@@ -4,7 +4,7 @@
 echo "<div id='bottom-bar' class=''>";
 echo '<a id="botaoVoltar" href="javascript:history.go(-1)" title="Voltar para página anterior"><i class="fas fa-chevron-circle-left"></i></a>';
 echo "<div id='copyright-text'>";
-echo "&copy;2018-2020 website by <a href='https://www.linkedin.com/in/luís-cereda'>Luis Cereda</a>";
+echo "&copy;2018-2021 website by <a href='https://www.linkedin.com/in/luís-cereda'>Luis Cereda</a>";
 echo "</div>";
 echo "<a id='security-test' href='https://seal.beyondsecurity.com/vulnerability-scanner-verification/confusa.top'><img src='https://seal.beyondsecurity.com/verification-images/confusa.top/vulnerability-scanner-2.gif' alt='Website Security Test'></a>";
 echo "</div>";
