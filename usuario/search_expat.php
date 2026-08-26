0cce8247c48baede259caf45c4395fb298b2ddd5
Luis Cereda <62411006+lhsaia@users.noreply.github.com>
2026-08-25 23:26:52 -0300

    Feat: new search climate, expat, stadium

---
+++ usuario/search_expat.php
@@ -0,0 +1,30 @@
+﻿<?php  
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/config/session.php';
+	
+	if(isset($_SESSION['user_id']) && $_SESSION['user_id'] <> 0){
+		$user = $_SESSION['user_id'];
+	} else {
+		$user = 0;
+	}
+
+    $item_pesquisado = isset($_POST['searchText']) ? $_POST['searchText'] : '';
+
+	include_once($_SERVER['DOCUMENT_ROOT']."/config/database.php");
+	include_once($_SERVER['DOCUMENT_ROOT']."/objetos/jogador.php");
+
+	$database = new Database();
+	$db = $database->getConnection();
+	$jogador = new Jogador($db);
+
+    $stmt = $jogador->readAllExpatAjax($item_pesquisado, $user);
+    $return_arr = [];
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $row['posicoesFormatadas'] = $jogador->listaPosicoes($row['StringPosicoes'] ?? '');
+        $row['podeRepatriar'] = $jogador->testeInatividade($row['ID']);
+        $return_arr[] = $row;
+    }
+    
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($return_arr);
+?>
---
+++ usuario/search_stadium.php
@@ -0,0 +1,23 @@
+﻿<?php  
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/config/session.php';
+	
+	if(isset($_SESSION['user_id']) && $_SESSION['user_id'] <> 0){
+		$user = $_SESSION['user_id'];
+	} else {
+		$user = 0;
+	}
+
+    $item_pesquisado = isset($_POST['searchText']) ? $_POST['searchText'] : '';
+
+	include_once($_SERVER['DOCUMENT_ROOT']."/config/database.php");
+	include_once($_SERVER['DOCUMENT_ROOT']."/objetos/estadio.php");
+
+	$database = new Database();
+	$db = $database->getConnection();
+	$estadio = new Estadio($db);
+
+    $stmt = $estadio->readAllAjax($item_pesquisado, $user);
+    $return_arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo json_encode($return_arr);
+?>
